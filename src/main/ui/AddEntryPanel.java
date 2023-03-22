da17bf075b20c75ad87875f1c9eccbbc7ce48f9e
Janie H <janieholzmann@hotmail.com>
2023-03-22 12:29:09 -0700

    added calcualtor panel and edited layouts of some panels

--- src/main/ui/AddEntryPanel.java
+++ src/main/ui/AddEntryPanel.java
@@ -4,6 +4,7 @@ import model.BloodSugarReading;
 import model.LogBook;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class AddEntryPanel extends ManagerPanel {
     private JButton add;
@@ -38,6 +39,7 @@ public class AddEntryPanel extends ManagerPanel {
     // EFFECTS: opens a JPanel with places to add data to add new logbook entry
     public AddEntryPanel(ManagerGUI gui) {
         super(5, 2, gui);
+        setPreferredSize(new Dimension(500, 200));
         valueLabel = new JLabel("Value in mmol/L");
         yearLabel = new JLabel("Year");
         monthLabel = new JLabel("Month");
--- src/main/ui/CalculatorPanel.java
+++ src/main/ui/CalculatorPanel.java
@@ -1,13 +1,76 @@
 package ui;
 
-public class CalculatorPanel extends ManagerPanel {
+import model.InsulinCalculator;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CalculatorPanel extends ManagerPanel implements ActionListener {
+    private InsulinCalculator calculator;
+    private JButton calculate;
+
+    private JTextField currentBloodSugar;
+    private JTextField insulinCarbRatio;
+    private JTextField base;
+    private JTextField carbs;
+    private JTextField insulinSensitivity;
+
+    private JLabel currentSugarLabel;
+    private JLabel ratioLabel;
+    private JLabel baseLabel;
+    private JLabel carbLabel;
+    private JLabel sensitivityLabel;
 
     public CalculatorPanel(ManagerGUI gui) {
-        super(2, 1, gui);
+        super(2, 5, gui);
+        setPreferredSize(new Dimension(500, 350));
+        currentSugarLabel = new JLabel("Current Blood Sugar (mmol/L):");
+        ratioLabel = new JLabel("Insulin to carb ratio (units per 1 gram):");
+        baseLabel = new JLabel("Base Insulin:");
+        carbLabel = new JLabel("Grams of carbs in food:");
+        sensitivityLabel = new JLabel("Insulin Sensitivity Factor (units per mmol/L):");
+        currentBloodSugar = new JTextField(5);
+        insulinCarbRatio = new JTextField(5);
+        base = new JTextField(5);
+        carbs = new JTextField(5);
+        insulinSensitivity = new JTextField(5);
+
+        calculate = new JButton("Calculate");
+        calculate.setActionCommand("calculate");
+        calculate.addActionListener(this);
+
+        addReturnMainMenuButton();
+        addEverything();
     }
 
     @Override
     protected void addEverything() {
+        add(currentSugarLabel);
+        add(currentBloodSugar);
+        add(ratioLabel);
+        add(insulinCarbRatio);
+        add(baseLabel);
+        add(base);
+        add(carbLabel);
+        add(carbs);
+        add(sensitivityLabel);
+        add(insulinSensitivity);
+        add(calculate);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("calculate")) {
+            calculator = new InsulinCalculator(Double.parseDouble(currentBloodSugar.getText()),
+                    Double.parseDouble(carbs.getText()), Integer.parseInt(base.getText()),
+                    Double.parseDouble(insulinCarbRatio.getText()), Double.parseDouble(insulinSensitivity.getText()));
+            int insulinToGive = calculator.calculateInsulin();
+            add(new JLabel("Insulin to give: " + insulinToGive + " units"));
+            SwingUtilities.updateComponentTreeUI(this);
+        }
 
     }
 }
